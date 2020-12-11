7cc04bab4538d55ebb33f0c370debbc1a55dda3e
zechlau14 <73336747+zechlau14@users.noreply.github.com>
2020-12-11 19:14:09 +0800

    Update and rename indoors-C-vs-t.py to C-vs-t.py

--- C-vs-t.py
+++ C-vs-t.py
@@ -3,12 +3,10 @@ import matplotlib.pyplot as plt
 from scipy.signal import convolve
 
 #PDE parameters
-v = 0.15 # 0.15m/s
-R = 1 #0.5
-Q = [0.000033, 0.0002, 0.000833, 0.00167]
-K = [0.00088, 0.0053, 0.0220, 0.0441] 
-#V = 192
-#K = Q * V * (2*V)**(-1/3)
+v = 0.15 
+R = 1
+Q = [0.000033, 0.0002, 0.00083, 0.0017]
+K = [0.00088, 0.0053, 0.022, 0.044] 
 
 #Domain
 l = 8 
@@ -24,7 +22,7 @@ hour = 8
 
 # graph's axis
 t_end = 60*60 * hour
-delta_t = 0.3 # 0.025
+delta_t = 0.3
 n_t = int(t_end/delta_t) + 1
 t = np.linspace(0.1,t_end+0.1,n_t)
 
@@ -35,7 +33,6 @@ S = np.full(len(t), R)
 
 k = int(34*hour) #nodes 34 for 1h
 
-#for i in range(1,2):
 for i in range(len(Q)):
     C_y = np.exp(-((y-y_o)**2)/(4*K[i]*t)) + np.exp(-((y+y_o)**2)/(4*K[i]*t))
     C_x = np.exp(-((x-x_o-v*t)**2)/(4*K[i]*t)) + np.exp(-((x+x_o+v*t)**2)/(4*K[i]*t))
@@ -46,10 +43,8 @@ for i in range(len(Q)):
         C_x = C_x + np.exp(-((x-x_o -v*t + 2*n*b)**2)/(4*K[i]*t)) + np.exp(-((x+x_o+v*t - 2*n*b)**2)/(4*K[i]*t))
     I = 1/(4*np.pi*K[i]*t) * C_y * C_x * np.exp(-Q[i]*t)
     C[i] = convolve(S,I)[0:len(t)] * delta_t * 0.5
-    D = convolve(S,I,mode='valid')*delta_t*0.5
-    print(D)
 
-n_s = int(60/delta_t)
+#n_s = int(60/delta_t)
 t = t / 60
 
 #plot 
@@ -63,7 +58,6 @@ plt.plot(t,C[3],linestyle='dotted',color='blue')
 #plt.loglog(t[n_s:],C[3][n_s:],linestyle='dotted',color='blue') 
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
-#plt.xlim([1,10])
-plot_name = "./plots/C-vs-t-log-XY" +str(x) + str(y) + ".png"
+plot_name = "./plots/C-vs-t-XY" +str(x) + str(y) + ".png"
 plt.savefig(plot_name)
 plt.show()
